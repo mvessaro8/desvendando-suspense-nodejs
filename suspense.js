fca9ec1c547224660c313e974e660af73fb6e5e2
Valter Neto <valter.neto32@gmail.com>
2020-01-09 19:11:34 -0300

    Primeira session de dojo

--- suspense.js
+++ suspense.js
@@ -1,5 +1,29 @@
-const dummyTest = () => "Hello Test"
+const resultado = [2,3,4];
+
+const verificaTeoria = (teoria) => {
+    let erros = []
+    if(teoria[0] !== resultado[0]) {
+        erros.push(1)
+    }
+    if(teoria[1] !== resultado[1]) {
+        erros.push(2)
+    }
+    if(teoria[2] !== resultado[2]) {
+        erros.push(3)
+    }
+
+    if () {
+
+    }
+    
+    if (!erros) {
+        return 0
+    } else {
+        return erros[parseInt((Math.random()*(0-2) + (erros.length-1)).toFixed(0))]
+    }
+}
 
 module.exports = {
-    dummyTest
+    resultado,
+    verificaTeoria,
 }
--- suspense.test.js
+++ suspense.test.js
@@ -1,6 +1,16 @@
-const { dummyTest } = require('./suspense')
+const resultadoTeste = [2,3,4];
+
+const { resultado, verificaTeoria } = require('./suspense');
 
 
-test('Dummy test', () => {
-    expect( dummyTest() ).toBe("Hello Test")
-})
+test('Verifica se o valor informado é igual ao valor do res', () => {
+    expect(resultado).toStrictEqual(resultadoTeste)
+});
+
+test('Verifica se a teoria está correta', () => {
+    expect(verificaTeoria([2,3,4])).toEqual(0);
+});
+
+test('Verificar se o input é uma possível resposta', () => {
+    expect(verificaTeoria([3,3,4])).toStrictEqual(1);
+});
