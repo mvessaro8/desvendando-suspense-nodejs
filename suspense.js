798d2727956a3498f0daa559476b3aeeaa37e1b9
Valter Neto <valter.neto32@gmail.com>
2020-09-15 19:05:47 -0300

    Dojo de Setembro 2020

--- suspense.js
+++ suspense.js
@@ -1,27 +1,48 @@
-const resultado = [2,3,4];
+const suspeitos = [
+    {
+        id: 1,
+        nome: 'Charles B. Abbage'
+    },
+    {
+        id: 2,
+        nome: 'Donald Duck Knuth'
+    },
+    {
+        id: 3,
+        nome: 'Ada L. Ovelace'
+    },
+    {
+        id: 4,
+        nome: 'Alan T. Uring'
+    },
+    {
+        id: 5,
+        nome: 'Ivar J. Acobson'
+    },
+    {
+        id: 6,
+        nome: 'Ras Mus Ler Dorf'
+    }
+];
 
-const verificaTeoria = (teoria) => {
-    let erros = []
-    if(teoria[0] !== resultado[0]) {
-        erros.push(1)
-    }
-    if(teoria[1] !== resultado[1]) {
-        erros.push(2)
-    }
-    if(teoria[2] !== resultado[2]) {
-        erros.push(3)
-    }
+let getSuspeitoById = (id) => {
+    return suspeitos.filter(sus => sus.id === id)[0];
+}
 
-    if (erros.length === 0) {
-        return 0
-    } else  if (erros.length === 1 && erros[0] === 1) {
-        return 1 
+const evidencias = [2,3,4];
+
+let validarTeoria = (arr) => {
+    if (arr[0] !== evidencias[0]) {
+        return 1;
+    } else if (arr[1] !== evidencias[1]) {
+        return 2;
+    } else if (arr[2] !== evidencias[2]) {
+        return 3;
     } else {
-        return parseInt((Math.random()*(0-2) + (resultado.length-1)).toFixed(0));
+        return 0;
     }
 }
 
-module.exports = {
-    resultado,
-    verificaTeoria,
-}
+module.exports.suspeitos = suspeitos;
+module.exports.getSuspeitoById = getSuspeitoById;
+module.exports.validarTeoria = validarTeoria;
